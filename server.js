4d5f0a4a0c0baa3b632e28aae1384812322c26c9
shakill2 <92432187+shakill2@users.noreply.github.com>
2025-03-17 12:28:45 +0200

    Update server.js

--- server.js
+++ server.js
@@ -1,11 +1,8 @@
 const express = require("express");
 const app = express();
-const PORT = process.env.PORT || 3000;
 
 app.use((req, res) => {
     res.status(401).json({ error: "Unauthorized" });
 });
 
-app.listen(PORT, () => {
-    console.log(`Server running on port ${PORT}`);
-});
+module.exports = app; // Export instead of app.listen()
